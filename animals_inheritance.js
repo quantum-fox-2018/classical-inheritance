922d4a0e60760895aec442a992f9e6bc58dcec69
cantdocpp <cihuyman95@gmail.com>
2018-03-12 20:57:24 +0700

    first push

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,41 @@
 "use strict"
+
+class Animal {
+  constructor(name, num_legs, invisible, laser) {
+    this.name = name;
+    this.num_legs = num_legs;
+    this.superpower = new SuperPower(invisible);
+  }
+}
+
+class Rabbit extends Animal {
+  constructor(name, num_legs, invisible, laser) {
+    super(name, num_legs, invisible, laser);
+  }
+}
+
+class Chimpanzee extends Animal {
+  constructor(name, num_legs, invisible) {
+    super(name, num_legs, invisible);
+  }
+}
+
+class SuperPower {
+  constructor(invisible, laser) {
+    this.invisible = this.be_invisible(invisible)
+    this.laser = this.use_laser_vision(laser)
+  }
+
+  be_invisible(invisible) {
+    return invisible
+  }
+
+  use_laser_vision(laser) {
+    return laser;
+  }
+}
+
+let rabbit = new Animal('white rabbit', 4, true, false)
+// console.log(rabbit.name)
+// console.log(rabbit.speed)
+console.log(rabbit)
