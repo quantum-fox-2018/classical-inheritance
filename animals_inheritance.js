59f45f1f13df5b32ff5a81ca40c1b9b119551a0f
Rafie Gilang <gilang0293@gmail.com>
2018-03-12 20:23:50 +0700

    sudah selesai classic-inheritance realese 1

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -42,20 +42,20 @@ class Bat extends Animal{
 var animal = new Animal('any');
 var sup = new SuperPower();
 
-console.log('animal' + ': species => ' + animal.species + ' / can fly =>' + animal.can_fly + '\n');
+console.log('animal' + ': species => ' + animal.species + ' / can fly => ' + animal.can_fly + '\n');
 
 var frog = new Frog('amphibian','small',sup);
 animal.can_fly = 'cannot';
 
-console.log('animal' + ': species =>' +animal.species + ' / can fly =>' + animal.can_fly);
-console.log('frog' + ': species =>' +frog.species + ' / can fly =>' + frog.can_fly + ' / size =>' + frog.size + '\n');
+console.log('animal' + ': species => ' +animal.species + ' / can fly => ' + animal.can_fly);
+console.log('frog' + ': species => ' +frog.species + ' / can fly => ' + frog.can_fly + ' / size =>' + frog.size + '\n');
 
 var bat = new Bat('mammal','not all',sup);
 bat.can_fly = 'can';
 animal.suck_blood = 'because they are vampire'
 
-console.log('animal' + ': species =>' +animal.species + ' / can fly =>' + animal.can_fly  + ' / ' + animal.suck_blood);
-console.log('bat' + ': species =>' +bat.species + ' / can fly =>' + bat.can_fly + ' / ' + bat.suck_blood + '\n');
+console.log('animal' + ': species => ' +animal.species + ' / can fly => ' + animal.can_fly  + ' / suck blood => ' + animal.suck_blood);
+console.log('bat' + ': species => ' +bat.species + ' / can fly => ' + bat.can_fly + ' / suck blood => ' + bat.suck_blood + '\n');
 
 console.log(frog.sup.use_laser_vision());
 console.log(bat.sup.be_invisible());
