8558e6952c036524f77495cab18952edf05efeee
fastabiqul1993 <fastabiqul1993@gmail.com>
2018-03-12 21:03:27 +0700

    Adhyfastabiqul

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,71 @@
 "use strict"
+
+class Hewan {
+
+    constructor(warna, habitat) {
+
+        this.warna = warna
+        this.habitat = habitat
+        this.SuperPower = new SuperPower()
+    }
+}
+
+class SuperPower {
+
+    constructor(superPower) {
+
+        this.superPower = superPower
+    }
+
+    highJump() {
+
+        return 'High Jump'
+    }
+
+    tikungMaster() {
+
+        return 'Tikung Master'
+    }
+
+    glow() {
+
+        return 'Glow in the dark'
+    }
+}
+
+class Belalang extends Hewan {
+
+    constructor(warna, habitat) {
+
+        super(warna, habitat)
+    }   
+}
+
+class Rubah extends Hewan {
+
+    constructor(warna, habitat) {
+
+        super(warna, habitat)
+    }   
+}
+
+class LandCrocodille extends Hewan {
+
+    constructor(warna, habitat) {
+
+        super(warna,habitat)
+    }
+}
+
+class Komodo extends Hewan {
+
+    constructor(warna, habitat) {
+
+        super(warna, habitat)
+    }   
+}
+
+let pilihHewan = new LandCrocodille ('Abstract', 'Gang senggol')
+
+// console.log(pilihHewan)
+console.log(pilihHewan.SuperPower.tikungMaster())
