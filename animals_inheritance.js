b64adc383d644793b649aa1dd2dbb87189ae410b
barestu <fheartnet@gmail.com>
2018-03-12 19:14:20 +0700

    Tugas Classical Inheritance

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,55 @@
 "use strict"
+
+class  Animal {
+  constructor() {
+    this.num_legs = 2;
+    this.is_warm_blooded = true;
+    this.superpower = new SuperPower();
+  }
+}
+
+class SuperPower {
+  constructor() {
+
+  }
+
+  use_laser_visions() {
+    console.log('Use Laser Visions: it is very effective!');
+  }
+
+  be_invisible() {
+    console.log('Invisible ability activated!');
+  }
+}
+
+class Fox extends Animal {
+  constructor(num_legs) {
+    super();
+    this.num_legs = num_legs;
+  }
+}
+
+class Bird extends Animal {
+  constructor(is_warm_blooded) {
+    super();
+    this.is_warm_blooded = is_warm_blooded;
+  }
+}
+
+class Spider extends Animal {
+  constructor(num_legs) {
+    super();
+    this.num_legs = num_legs;
+  }
+}
+
+let fox = new Fox(4);
+let bird = new Bird(false);
+let spider = new Spider(8);
+
+console.log(fox);
+fox.superpower.use_laser_visions();
+//console.log(bird);
+console.log();
+console.log(spider);
+spider.superpower.be_invisible();
